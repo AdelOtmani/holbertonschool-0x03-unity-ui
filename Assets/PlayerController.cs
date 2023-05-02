e9cdf572782209a1e4f78aacee45b20e85ddbf69
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-05-02 22:11:44 +0200

    cloning project

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float speed = 1500f;
+	public Rigidbody Rb;
+    private int score = 0;
+    public int health = 5;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+
+        if (Input.GetKey("d"))
+        {
+            Rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            Rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            Rb.AddForce(0, 0, speed * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            Rb.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            Debug.Log("Score: " + score.ToString());
+        }
+        if (other.gameObject.CompareTag("Trap"))
+        {
+            health--;
+            Debug.Log("Health: " + health.ToString());
+        }
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            Debug.Log("You win!");
+        }
+    }
+}
