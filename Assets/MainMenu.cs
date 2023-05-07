bd79df72c07ea2fa903cac082249b38debafb76a
AdelOtmani <105648440+AdelOtmani@users.noreply.github.com>
2023-05-07 20:40:03 +0200

    Task 11

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayMaze()
+    {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
