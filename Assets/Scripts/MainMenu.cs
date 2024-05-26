6ea2cb5c3b400a38b0383fb39af28325821266b3
AdelOtmani <5023@holbertonschool.com>
2024-05-26 20:48:05 +0200

    task 9 to 11

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayMaze()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+	public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
